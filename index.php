88c38c4296782847103a42156a7c3960db6df046
jhrobert <jack.robert@uvm.edu>
2016-12-06 20:20:52 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -5,11 +5,9 @@ include "top.php";
 
 <article>
     <h2> Welcome! </h2>
-</article>
 
-<p> <!--Information about site --></p>
+    <p class="paragraph">Have you ever wanted to know how to make a website? Or what the difference between HTML, CSS, and PHP are? Well you might have found just the right place! Whether you are a novice web designer or a student preparing for a final exam, you are sure to find some information and tips that are helpful for your success in coding sites.</p>
 
-<!-- put something in aside element and float right -->
 <figure>
     <img></img> <!-- something goofy/ironic -->
     <figcaption></figcaption>
